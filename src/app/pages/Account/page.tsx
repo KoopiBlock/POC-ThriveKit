29d08bf0a50783c48b062a4d4930366beaaca4fd
MaDnNeSsKoOpi <96216773+MaDnNeSsKoOpi@users.noreply.github.com>
2024-06-25 13:44:19 +0300

    commit till compile 2

--- src/app/pages/Account/page.tsx
+++ src/app/pages/Account/page.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import Link from 'next/link'
 
 import { GiCoins } from "react-icons/gi";
--- src/app/pages/RefferalPage/page.tsx
+++ src/app/pages/RefferalPage/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 import {
     Drawer,
